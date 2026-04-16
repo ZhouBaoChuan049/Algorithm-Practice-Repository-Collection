b1d28ae863e4473c2e612352bf5148f0b74c173c
ZhouHan20 <3450014365@qq.com>
2026-04-16 08:03:42 +0800

    算法实战论前缀和篇（博客同名）/一维前缀和模板

---
+++ 算法实战论前缀和篇（博客同名）/一维前缀和模板.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+//int main() {
+//    int l = 0, r = 0, n = 0, m = 0;
+//    int arr[10] = { 0 };
+//    int mp[10] = { 0 };
+//    vector<int> v;
+//    cin >> m >> n;
+//    for (int i = 1; i <= n + 1; i++)
+//    {
+//        cin >> arr[i];
+//    }
+//    for (int j = 1; j <= n + 1; j++)
+//    {
+//        mp[j] = mp[j - 1] + arr[j];
+//    }
+//    while (m)
+//    {
+//        cin >> l >> r;
+//        cout << (mp[r] - mp[l - 1]) << endl;
+//        m--;
+//    }
+//}
